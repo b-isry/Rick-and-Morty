2009e1f163b6d78b243752c7c8f3c7ad24fe32c8
b_isry <bisratt1995@gmail.com>
2024-05-20 23:05:45 +0300

    Editing link to github

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="bg-lime-900 text-white py-4">
     <div class="container mx-auto text-center">
       <p>&copy; 2024 Rick and Morty</p>
-      <p class="mt-2"><a href="https://github.com/b-isry" target="blank" class="underline hover:text-green-400">GitHub</a></p>
+      <p class="mt-2"><a href="https://github.com/b-isry/Rick-and-Morty/tree/main" target="blank" class="underline hover:text-green-400">GitHub</a></p>
     </div>
   </footer>
 </template>
